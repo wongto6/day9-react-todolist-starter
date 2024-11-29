bc33915ab98364655fccc91cf7da2eddeebbbc44
WONGTO6 <wongto6@oocl.com>
2024-11-29 11:03:50 +0800

    feat: add link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {createContext, useReducer} from "react";
 import './App.css';
 import TodoList from "./components/todo/TodoList";
 import {initialState, todoReducer} from "./context/todoReducer";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import {Navigate, useNavigate} from "react-router-dom";
 import MultipleCounter from "./components/counter/MultipleCounter";
 import NotFoundPage from "./components/NotFoundPage";
@@ -17,6 +17,10 @@ function App() {
         <div className="App">
             <TodoContext.Provider value={{state, dispatch}}>
                 <Router>
+                    <nav>
+                        <Link to={'/'}>Home</Link> |
+                        <Link to={'/done'}>Done-List</Link>
+                    </nav>
                     <Routes>
                         <Route path='/' element={<TodoList/>}/>
                         <Route path='/done' element={<DoneList/>}/>
