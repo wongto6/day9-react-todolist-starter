dd88d024adafbeba88e0ff63db85a92506f9d67f
WONGTO6 <wongto6@oocl.com>
2024-11-29 09:12:59 +0800

    feat: hide Add the things you need to do today if have contents

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,18 @@
 import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator";
+import {useContext} from "react";
+import {TodoContext} from "../App";
 
 const TodoList = () => {
+
+    const {state} = useContext(TodoContext)
+
     return (
         <div>
             <div className="bottom-space">
                 <span className="title">Todo List</span>
             </div>
-            <span className="top-space">Add the things you need to do today...</span>
+            {state.length > 0 ? null : <span className="top-space">Add the things you need to do today...</span>}
             <TodoGroup/>
             <TodoGenerator/>
         </div>
